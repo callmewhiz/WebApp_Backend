86055aaf06448b724f852e8c6921d560acb20952
callmewhiz <gideon.b@turing.com>
2025-12-31 21:07:38 +0100

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,3 +1,4 @@
 fn main() {
-  println!("This is turing task");
+  println!("Welcome to the world of Rust");
+  println!("Rust is a system programming with the speed of C and the simplicity of Python");
 }
